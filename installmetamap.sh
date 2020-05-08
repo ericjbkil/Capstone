13a6a121ba2ca24cf5b7bab8135e1ce1833834e9
ericjbkil <59625638+ericjbkil@users.noreply.github.com>
2020-05-07 21:58:51 -0700

    Update installmetamap.sh

--- installmetamap.sh
+++ installmetamap.sh
@@ -1,12 +1,3 @@
-pip install pdfminer
-
-#make sure you're in the right directory (folder with your pdfs per patient)
-
-#general layout for parsing of PDFs into plain text
-pdf2txt.py -o name_of_output_file input_file.pdf
-
-#removing ASCII characters
-tr -cd '\11\12\15\40-\176' < file-with-binary-chars > clean-file
 
 ####metamap installation: from https://metamap.nlm.nih.gov/Installation.shtml #### 
 # To extract the MetaMap distribution, use the following bunzip2 and tar commands substituting the appropriate 
